ecd09f329ee2d89b362839ffb58e8e6562a3a243
Yongye Tan <yongyetan1209@gmail.com>
2023-01-21 22:30:27 -0500

    Add feature on linear gradient and button on flag and alert window

--- TaylorSwift/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ TaylorSwift/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,10 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia",
+                     "Spain", "UK", "US"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
     var body: some View {
         ZStack {
-            Color(red: 1, green: 2, blue: 0.8)
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]), startPoint: .top, endPoint: .bottom)
+                .ignoresSafeArea()
+            VStack (spacing: 30){
+                VStack {
+                    Text("Tap the flag of ")
+                        .foregroundColor(.white)
+                        .font(.subheadline.weight(.heavy))
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle.weight(.semibold))
+                }
+                
+                ForEach(0..<3) {number in
+                    Button {
+                        // flag was tagged
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .renderingMode(.original)
+                            .shadow(radius: 10)
+                    }
+                }
+            }
         }
+        // it only presents when showingScore = true
+        // so find a way to disable and able it
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askMoreQuestion)
+            } message: {
+                Text("Your score is ???")
+            }
+        }
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correct"
+        } else {
+            scoreTitle = "Incorrect"
+        }
+        
+        showingScore = true
+    }
+    
+    func askMoreQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
     }
 }
 
