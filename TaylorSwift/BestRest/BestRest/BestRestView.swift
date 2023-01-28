7b2f60eadbd16d9a28315a9f742a968719c1a741
Yongye Tan <yongyetan1209@gmail.com>
2023-01-28 18:01:52 -0500

    Add section to each Stack and replace a stepper with a picker

--- TaylorSwift/BestRest/BestRest/BestRestView.swift
+++ TaylorSwift/BestRest/BestRest/BestRestView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import CoreML
 
 struct BestRestView: View {
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeup
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
@@ -17,29 +17,63 @@ struct BestRestView: View {
     @State private var alertMessage = ""
     @State private var showingAlert = false
     
+    // default wake up is 8 am
+    // defaultWakeUp should belong to the struct, not to a particular instance
+    // share among all the struct
+    static var defaultWakeup : Date {
+        var components = DateComponents()
+        components.hour = 8
+        components.minute = 0
+        
+        // if default does not work, then new default would be the current now
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+    
     
     
     var body: some View {
         NavigationView {
-            VStack {
-                Text("When do you want to wake up")
-                    .font(.headline)
+            Form { // try VStack(alignment: .leading, spacing: 10)
+                Section { // try Form
+//                    Text("When do you want to wake up??")
+//                        .font(.headline)
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                } header: {
+                    Text("When do you want to wake up??")
+                        .font(.headline)
+                }
                 
-                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
+                Section { // VStack(alignment: .leading, spacing: 10)
+//                    Text("Desired amount of sleep")
+//                        .font(.headline)
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                } header: {
+                    Text("Desired amount of sleep")
+                        .font(.headline)
+                }
                 
-                Text("Desired amount of sleep")
-                    .font(.headline)
+                Section { // VStack(alignment: .leading, spacing: 10)
+                    // Text("Daily coffee intake")
+                    //   .font(.headline)
+                    
+                    // Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                    
+                    Picker("Coffee", selection: $coffeeAmount) {
+                        ForEach(0..<9) {
+                            Text($0 > 1 ? "\($0) cups" : "\($0) cup")
+                        }
+                    }
+            
+                    
+                } header: {
+                    Text("Daily coffee intake")
+                        .font(.headline)
+                }
                 
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-                
-                Text("Daily coffee intake")
-                    .font(.headline)
-                
-                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
             }
             .navigationTitle("BetterRest")
-            // put the button on the top right of the screen 
+            // put the button on the top right of the screen
             .toolbar {
                 Button("Calculate", action: calculateBedTime)
             }
@@ -53,7 +87,7 @@ struct BestRestView: View {
             }
         }
     }
-
+    
     func calculateBedTime() {
         do {
             // this is assuming that we have a trained .mlmodel file
