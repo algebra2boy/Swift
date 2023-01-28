24c35f61ebae4af8cad49bf48f3cfebde523d9dc
Yongye Tan <yongyetan1209@gmail.com>
2023-01-28 00:03:23 -0500

    set up ML model to predict the bed time

--- TaylorSwift/BestRest/BestRest/BestRestView.swift
+++ TaylorSwift/BestRest/BestRest/BestRestView.swift
@@ -6,11 +6,17 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct BestRestView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
     
     
     var body: some View {
@@ -33,13 +39,46 @@ struct BestRestView: View {
                 Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
             }
             .navigationTitle("BetterRest")
+            // put the button on the top right of the screen 
             .toolbar {
                 Button("Calculate", action: calculateBedTime)
             }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("Okay"){
+                    print("alert button is being pressed")
+                    print(alertMessage)
+                }
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
+
     func calculateBedTime() {
-        
+        do {
+            // this is assuming that we have a trained .mlmodel file
+            let configuration = MLModelConfiguration()
+            // using this particular sleepCalculator omdel
+            let MLmodel = try sleepCalculator(configuration: configuration)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = (components.minute ?? 0) * 60 * 60
+            
+            let prediction = try MLmodel.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            alertTitle = "Your ideal bedtime"
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+        } catch {
+            
+            alertTitle = "error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime."
+        }
+        showingAlert = true
     }
     
 }
