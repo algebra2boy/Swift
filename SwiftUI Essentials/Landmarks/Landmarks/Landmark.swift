b4e99adf2ca325a697e198f2477ab39bf27fa21c
Yongye Tan <yongyetan1209@gmail.com>
2023-06-24 22:37:25 -0400

    Create landmark struct

---
+++ SwiftUI Essentials/Landmarks/Landmarks/Landmark.swift
@@ -0,0 +1,39 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Yongye Tan on 6/24/23.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Codable, Hashable {
+    // did not include all the property from the json
+    // only get the ones that we need
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    // We make the property private because users of the Landmarks structure care only about the image itself.
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinate
+    var locationCoordination: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+    
+    struct Coordinate: Codable, Hashable {
+        var longitude: Double
+        var latitude : Double
+    }
+}
