08d11e16329e0e8cb4c20420fdfabb22b9efaba6
Yongye Tan <yongyetan1209@gmail.com>
2023-07-10 14:11:38 -0400

    Initial Commit - Hike App

---
+++ MasterClass/HikeApp/HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by Yongye Tan on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
