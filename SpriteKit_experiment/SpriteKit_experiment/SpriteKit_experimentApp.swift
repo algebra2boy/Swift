4de6c2e5a0788189f013326128b40b911d7eb392
Yongye Tan <yongyetan1209@gmail.com>
2024-04-22 23:26:20 -0400

    Add spritekit project

---
+++ SpriteKit_experiment/SpriteKit_experiment/SpriteKit_experimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKit_experimentApp.swift
+//  SpriteKit_experiment
+//
+//  Created by local-user on 4/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKit_experimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
