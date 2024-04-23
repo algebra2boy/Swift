9fa47d9261a29e58df902efcb08af3f5ceabff7e
Yongye Tan <yongyetan1209@gmail.com>
2024-04-22 23:51:30 -0400

    cool box stack on top of each other

--- SpriteKit_experiment/SpriteKit_experiment/ContentView.swift
+++ SpriteKit_experiment/SpriteKit_experiment/ContentView.swift
@@ -2,20 +2,41 @@
 //  ContentView.swift
 //  SpriteKit_experiment
 //
-//  Created by local-user on 4/22/24.
+//  Created by Yongye Tan on 4/22/24.
 //
 
 import SwiftUI
+import SpriteKit
 
+class GameScene: SKScene {
+    override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let box = SKSpriteNode(color: .red, size: CGSize(width: 50, height: 50))
+        box.position = location
+        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 50, height: 50))
+        addChild(box)
+    }
+}
+
+// A sample SwiftUI creating a GameScene and sizing it
+// at 300x400 points
 struct ContentView: View {
+    var scene: SKScene {
+        let scene = GameScene()
+        scene.size = CGSize(width: 300, height: 400)
+        scene.scaleMode = .fill
+        return scene
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        SpriteView(scene: scene)
+            .frame(width: 300, height: 400)
+            .ignoresSafeArea()
     }
 }
 
